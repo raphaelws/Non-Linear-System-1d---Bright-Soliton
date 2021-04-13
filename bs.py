e820fdfe695535e3a408a3073d97f8d12da28f16
raphaelws <44216544+raphaelws@users.noreply.github.com>
2021-04-13 15:56:18 -0300

    Update bs.py

--- bs.py
+++ bs.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 bs = np.loadtxt('groundstate.txt')
 plt.title('Bright Soliton')
-plt.xlim([-64,64])
+plt.xlim([-5,5])
+print(bs[:,1])
 plt.plot(bs[:,0], bs[:,1],linewidth=2,label=r"$|\psi(x)|^{2}$")
 plt.xlabel("x")
 plt.legend(loc=1, prop={'size': 10})
