268f6c9f631c296966ac18df52f3f8ec78b60533
raphaelws <44216544+raphaelws@users.noreply.github.com>
2021-04-13 15:55:39 -0300

    Update nls1d.c

--- nls1d.c
+++ nls1d.c
@@ -5,20 +5,24 @@
 #include <fftw3.h>
 
 #define PI 3.141592653589793
-#define x_min -64
-#define x_max 64
+#define x_min -40
+#define x_max 40
 #define dt 0.01*I
 #define dt2 0.01
-#define N 512 //power of 2, good for FFT
+#define N 1024
 #define x0 0
 #define time 100
+//time 0.17
 
 //run with gcc nls1d.c -o nls1d -lfftw3 -lm
 
 //if imaginary time dt = Idt, if real time dt = dt
 
-double complex Gaussian(double);
+
+//i*dpsi/dt = (-d^2/dx^2 + U(x) + g|psi|^2)psi
+double complex psi0(double);
 void normalize();
+void attvexp();
 void initialize();
 void write_file(FILE *);
 void evolve();
@@ -28,7 +32,6 @@ double complex V_exp[N];
 double complex T_exp[N];
 double complex psi[N];   
 double complex psip[N]; 
-double p0;
 double x_step;
 fftw_plan fft, ifft;    // fftw plan
 
@@ -37,20 +40,20 @@ int main()
 	x_step = (double)(x_max-x_min)/(double)N;
 	FILE *f;
 	int i;
-	double T, pl, pr,k=0;
+	double k=0;
 	
-	f = fopen("groundstate.txt", "w+");
+	f = fopen("groundstate.txt", "w+");;
 	
 	fft = fftw_plan_dft_1d(N, psi, psip, FFTW_FORWARD,  FFTW_ESTIMATE);
-	ifft = fftw_plan_dft_1d(N, psip, psip, FFTW_BACKWARD,  FFTW_ESTIMATE);
+	ifft = fftw_plan_dft_1d(N, psip, psi, FFTW_BACKWARD,  FFTW_ESTIMATE);
 	initialize();
 	while(k<=time){
 		k=k+dt2;
 		evolve();
-		printf("Steps take = %lf", k);
+		printf("time envolved = %lf", k);
 		printf("\n");
-	}
-		
+		attvexp();
+		}
 	
 	fftw_destroy_plan(fft);
 	fftw_destroy_plan(ifft);
@@ -63,19 +66,23 @@ int main()
 double V(double x)
 {
 	
-		return pow(x,2);
+		return x*x;
 }
 
 void initialize()
 {
 	int i;
-	double x, p;
+	double wr,wi,x, p;
 	for(i=0; i<N; i++){
 		x = x_min + x_step*(i+1);
 		//V_exp[i]=cexp(-I*V(x)*dt);
-		psi[i] = Gaussian(x);
-		V_exp[i]=cexp(I*psi[i]*conj(psi[i]*dt/2.0));
-		
+		psi[i] = psi0(x);
+		//+ signal for dark soliton, g<0 repulsive
+		//- signal for bright soliton, g>0 atractive
+		//wr = creal(psi[i]);
+		//wi = cimag(psi[i]);
+		//V_exp[i]=cexp(-100*I*(wr*wr+wi*wi)*dt/2.0);
+		attvexp();
 	}
 
 	for(i=0; i<N; i++){
@@ -87,6 +94,19 @@ void initialize()
 		T_exp[i] = cexp(-I*p*p*dt/2.0);
 	}
 	normalize();
+
+}
+
+void attvexp(){
+	int i;
+	double wr,wi,x;
+	for(i=0; i<N; i++){
+		//x = x_min + x_step*(i+1);
+		wr = creal(psi[i]);
+		wi = cimag(psi[i]);
+		V_exp[i]=cexp(-200*I*(wr*wr+wi*wi)*dt/2.0);
+		}
+		
 
 }
 
@@ -106,21 +126,24 @@ void write_file(FILE *f)
 	}
 }
 
-double complex Gaussian(double x)
+double complex psi0(double x)
 {
 	return exp(-(x*x));
+	return  1/cosh(x);
+	
 }
 
 void normalize()
-// This function nomalizes the wave packet numerically
 {
 	double ww[N];
 	double prod=0;
 	double A;
 	int i;
+
+	//my normalization
 	for(i=0; i<N; i++){
 		ww[i] = psi[i]*conj(psi[i]);
-	}
+ 	}
 	//trapezoidal integral
 	for(i=0; i<N-1; i++){
 		prod += (ww[i]+ww[i+1])*x_step/2.0;
@@ -129,6 +152,7 @@ void normalize()
 	for(i=0; i<N; i++){
 		psi[i] = psi[i]/A;
 	}
+	
 }
 
 void evolve()
@@ -136,6 +160,7 @@ void evolve()
 {
 	//envolve half nabla 2
 	int i;
+	double wr,wi,x;
 	for(i=0; i<N; i++){
 		psi[i] *= V_exp[i];
 	}
@@ -148,11 +173,15 @@ void evolve()
 	//inverse fft
 	fftw_execute(ifft);
 	
+	normalize();
+	attvexp();
+	
 	//envolve half nabla 2
 	for(i=0; i<N; i++){
 		psi[i] *= V_exp[i];
 	}
 	normalize();
+	
 }
 
 
